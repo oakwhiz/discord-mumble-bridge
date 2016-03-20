178b86d5f5030203b63bbd5a3ebc9ff1afa4c05f
oakwhiz <oakwhiz@minousoft.com>
2016-03-19 18:36:27 -0700

    Better discord file handling, better errors

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+// discord-mumble-bridge
+// by oakwhiz
+// licensed under the LGPL 2.1
+//
+// Chat bridge between Discord and Mumble
+////////////////////////////////////////////////////////////////////////////////
+
+///////////
+// Includes
+///////////
+
 var htmlToText = require('html-to-text');
 var marked = require('marked');
 var request = require('request');
@@ -7,17 +19,27 @@ var sharp = require('sharp');
 var dataUriToBuffer = require('data-uri-to-buffer');
 //var streamBuffers = require('stream-buffers');
 
-
-
 var Discord = require("discord.js");
-
 var discordClient = new Discord.Client();
 
-
 var mumble = require('mumble'),
-    fs = require('fs');
+var fs = require('fs');
 
-//remember to generate a self-signed keypair in PEM format, using OpenSSL, before running
+/////////////////////////
+// Magic config variables
+/////////////////////////
+
+// Discord
+var discordUser = "email@example.com";
+var discordPassword = "";
+var discordRoom = "room-name";
+
+// Mumble
+var mumbleUri = 'mumble://example.com';
+var mumbleRoom = "/Room Name/";
+
+// Before using, generate a PEM keypair for Mumble
+// using OpenSSL and place in the same directory.
 var options = {
     key: fs.readFileSync( 'key.pem' ),
     cert: fs.readFileSync( 'cert.pem' )
@@ -41,7 +63,7 @@ var mumbleConnection = null;
 
 var mumbleConnect = function() {
     console.log( 'Mumble connecting' );
-    mumble.connect( 'mumble://mumble.server.example.com', options, function ( error, connection ) {
+    mumble.connect( mumbleUri, options, function ( error, connection ) {
         if( error ) {
             setTimeout(mumbleConnect, 5000);
             console.log( 'Mumble connection error' );
@@ -62,9 +84,17 @@ var mumbleConnect = function() {
             connection.on( 'channelState', function (state) {
                 channels[state.channelId] = state;
             });
+            connection.on("error", mumbleDie);
         }
     });
 };
+
+var mumbleDie = function(error) {
+    console.log( 'Mumble error' );
+    console.log( error.name );
+    console.log( error.data );
+    process.exit(1);
+}
 
 var onInit = function() {
     console.log( 'Mumble connection initialized' );
@@ -77,7 +107,7 @@ var onReady = function() {
 
     // Connection is ready (?)
     mumbleRootChannel = mumbleConnection.channelByPath("/");
-    mumbleTargetChannel = mumbleConnection.channelByPath("/Mumble Room Here/");
+    mumbleTargetChannel = mumbleConnection.channelByPath(mumbleRoom);
     mumbleTargetChannel.join();
 };
 
@@ -128,14 +158,42 @@ var onMessage = function(message,user,scope) {
 
 var discordChannel = null;
 
+
+var discordConnectErr = function() {
+    setTimeout(discordConnect, 5000);
+    console.log( 'Discord connection error' );
+};
+
+var discordConnectSuccess = function() {
+    console.log( 'Discord connected' );
+}
+
+var discordConnect = function() {
+    console.log( 'Discord connecting' );
+    discordClient.login(discordUser, discordPassword).then(discordConnectSuccess).catch(discordConnectErr);
+    discordClient.setStatus("online")
+};
+
+var discordDie = function (error) {
+    console.log( 'Discord error' );
+    if (error) {
+        console.log( error );
+    }
+    process.exit(1);
+}
+
 discordClient.on("message", function(message){
     if (message.author != discordClient.user) {
         var relayMsg = null;
         console.log( 'Discord message' );
-        var relayMsg = message.author.username + " (Discord/" + message.channel.name + '): \n' + message.cleanContent;
-        relayMsg = marked(relayMsg);
-        //relayMsg = relayMsg.replace(/\n/g, "<br />"); //fix line breaks
-        mumbleConnection.user.channel.sendMessage(relayMsg);
+        
+        if (message.cleanContent !== "") {
+            relayMsg = message.author.username + " (Discord/" + message.channel.name + '): \n' + message.cleanContent;
+            relayMsg = marked(relayMsg);
+            //relayMsg = relayMsg.replace(/\n/g, "<br />"); //fix line breaks
+            mumbleConnection.user.channel.sendMessage(relayMsg);
+            console.log( 'Discord message sent' );
+        }
         
         if (typeof message.attachments[0] !== 'undefined')	{			// Say the URL of an attachment (discord only permits one per message)
             const attachment = message.attachments[0];
@@ -152,7 +210,7 @@ var discordProcessMsgAttach = function (relayMsg, attachUrl) {
     console.log( 'Discord attachment, requesting' );
     request({ url:attachUrl, encoding:null }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log( 'Discord attachment, request success' );
+            console.log( 'Discord attachment, download success' );
             sharp(body).resize(320, null).jpeg().quality(40).toBuffer((err, buffer, info) => {
             //sharp(body).resize(128, null).png().compressionLevel(9).toBuffer((err, buffer, info) => {
                 if (!err) {
@@ -172,39 +230,27 @@ var discordProcessMsgAttach = function (relayMsg, attachUrl) {
                     mumbleConnection.user.channel.sendMessage(relayMsg + "<br /><img src=\"" + attachDataUri + "\"></img>" );
                     console.log( 'Discord attachment, forwarding success' );
                 } else {
-                    mumbleConnection.user.channel.sendMessage("ERROR: Attachment is not an image");
+                    console.log( 'Discord attachment, not an image' );
+                    mumbleConnection.user.channel.sendMessage(relayMsg + "<br />ERROR: Attachment is not an image");
                 }
             });
         } else {
-            console.log( 'Discord attachment, request failed' );
+            console.log( 'Discord attachment, download failed' );
+            mumbleConnection.user.channel.sendMessage(relayMsg + "<br />ERROR: Attachment download failed");
         }
     });
 };
 
 discordClient.on("ready", function(){
-    discordChannel = discordClient.channels.get("name", "room-name-here");
+    discordChannel = discordClient.channels.get("name", discordRoom);
     //discordClient.joinVoiceChannel(discordChannel);
 });
 
-var discordConnect = function() {
-    console.log( 'Discord connecting' );
-    discordClient.login("address@email.example.com", "Pa55w0rd").then(discordConnectSuccess).catch(discordConnectErr);
-    discordClient.setStatus("online")
-};
+discordClient.on("disconnected", discordDie);
+discordClient.on("error", discordDie);
 
-var discordConnectErr = function() {
-    setTimeout(discordConnect, 5000);
-    console.log( 'Discord connection error' );
-};
 
-var discordConnectSuccess = function() {
-    console.log( 'Discord connected' );
-}
-
-var discordFailNotice = function(failure) {
-    console.log("Discord Failure Notice: " + failure)
-    discordClient.sendMessage(discordChannel, failure);
-}
+// Events and functions must be above this line
 
 discordConnect();
 mumbleConnect();
